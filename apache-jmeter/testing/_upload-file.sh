87606215a2e2caf9dac598dd5655851f297f7045
aaseev <alexander.y.aseev@gmail.com>
2015-09-22 07:57:00 +0300

    apache-jmeter/_upload-file.sh was updated to use new path to data file

--- apache-jmeter/testing/_upload-file.sh
+++ apache-jmeter/testing/_upload-file.sh
@@ -3,7 +3,7 @@
 SCRIPT_DIR="$(cd `dirname $0` && pwd -P)"
 
 # upload file for testing
-FILE_ID=`curl http://10.16.0.2:9080/cloudStorage/v1.0/DICOMEnvelope/upload -F "dicom_file=@${SCRIPT_DIR}/data/000009.dcm" 2>/dev/null | grep '"id":' | grep -P -o "\d+"`
+FILE_ID=`curl http://10.16.0.2:9080/cloudStorage/v1.0/DICOMEnvelope/upload -F "dicom_file=@${SCRIPT_DIR}/../../data/000009.dcm" 2>/dev/null | grep '"id":' | grep -P -o "\d+"`
 
 # dump uploaded file id to a file for future use
 echo "$FILE_ID" > $SCRIPT_DIR/.uploaded-file-id.txt
